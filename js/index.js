cdd6d5629d595c69879448ec5d45b94acc6e057c
Joni De Borger <joni.de.borger@ehb.be>
2021-03-24 11:56:09 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+"use strict";
+//import Figure from './Figure.js';
+//import Circle from './Circle.js';
+//import Rectangle from './Rectangle.js';
+//import Square from './Square.js';
+
+const form = {
+    changeSelect(e) {
+
+    },
+    init() {
+        console.log('Form init!');
+    },
+    inputElementsWithLabel(label, id) {
+
+    },
+    submitForm(e) {
+
+    }
+};
+
+form.init();
